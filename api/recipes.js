9079ccd001b85d2db57dc74b66d8ff682462e752
endLoads <166923792+endLoads@users.noreply.github.com>
2026-04-11 16:55:09 +0300

    Update recipes.js

--- api/recipes.js
+++ api/recipes.js
@@ -20,7 +20,7 @@ export default async function handler(req, res) {
         method: "POST",
         headers: {
           Authorization: `Bearer ${token}`,
-          "Notion-Version": "2022-06-28",
+          "Notion-Version": "2025-09-03",
           "Content-Type": "application/json",
         },
         body: JSON.stringify(body),
